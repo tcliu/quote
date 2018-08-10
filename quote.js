e12c3ec7a721278b61675470df6f545675f5a325
tcliu <tcliu@live.hk>
2018-08-11 01:32:31 +0800

    Add poll interval config

--- quote.js
+++ quote.js
@@ -4,8 +4,11 @@ const cheerio = require('cheerio')
 const moment = require('moment')
 const chalk = require('chalk')
 
+const DEFAULT_POLL_INTERVAL = 5
 const ETNET_URL = 'http://www.etnet.com.hk/www/eng/futures/index.php'
 const AASTOCKS_URL = 'http://www.aastocks.com/en/stocks/market/bmpfutures.aspx'
+
+const pollInterval = process.argv.length > 2 ? Number(process.argv[2]) : DEFAULT_POLL_INTERVAL
 
 function getUrl(url, converter) {
   const stime = new Date().getTime()
@@ -33,6 +36,7 @@ function getUrl(url, converter) {
 }
 
 function etnetHsiFutures() {
+  once(() => console.log(`Fetching HSI future quote from ${chalk.yellow(ETNET_URL)}`))
   return getUrl(ETNET_URL, $ => {
     const tag = $('.FuturesQuoteContent:nth-child(2) .FuturesQuoteNominal span')
     if (tag.length == 1) {
@@ -46,6 +50,7 @@ function etnetHsiFutures() {
 }
 
 function aastocksHsiFutures() {
+  once(() => console.log(`Fetching HSI future quote from ${chalk.yellow(AASTOCKS_URL)}`))
   return getUrl(AASTOCKS_URL, $ => {
     const tag = $('.font26.bold.cls.ff-arial')
     if (tag.length == 1) {
@@ -58,10 +63,18 @@ function aastocksHsiFutures() {
   })
 }
 
-function print(data) {
+function printPrice(data) {
   console.log(chalk.cyan(moment(data.time).format('YYYY-MM-DDTHH:mm:ss.SSS')), chalk.green(data.price))
 }
 
+function once(fn) {
+  if (!this.done) {
+    fn()
+    this.done = true
+  }
+}
+
+etnetHsiFutures().then(printPrice)
 setInterval(() => {
-  etnetHsiFutures().then(print)
-}, 5000)
+  etnetHsiFutures().then(printPrice)
+}, pollInterval*1000)
